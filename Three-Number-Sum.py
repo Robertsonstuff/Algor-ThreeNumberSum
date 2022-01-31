a843fce0b91b511c3834134c8f88a8f539fb3ec1
luke robertson <robertson.stuff@gmail.com>
2022-01-31 10:31:49 -0500

    Update Three-Number-Sum.py

--- Three-Number-Sum.py
+++ Three-Number-Sum.py
@@ -12,7 +12,6 @@ def threeNumberSum(array, targetSum):
         right = len(array) -1
         # this loop will iterate through the array, setting markers left and right. 'left' will start at the start and iterate through the length of the array up by 1 increment each time.
         # the right marker will stay at the last index as it has not been told to do otherwise.
-        print(array)
         while left < right:
             currentSum = array[i] + array[left] + array[right]
 # meaning: keep looping if the following is true (-8 + -6 + 12) = -2. Is left less than right? Yes it is, keep going to the if statement. [-8, -6, 1, 2, 3, 5, 6, 12] 
@@ -28,7 +27,7 @@ def threeNumberSum(array, targetSum):
                 right -= 1
 # otherwise, if the -2 > 0, index the right marker to the left to get a smaller answer.
 # Once the array has gone through both of the loops - both moving the markers back and forth in the while loop AND the currentSum has moved through the array in the for loop. The completed array with print with all of the triplets that reached the targetSum 
-    return triplets
+    return (f"{triplets} \nThese are the groups of three numbers within the array that add up to the target number.")
 
 blah = threeNumberSum([12,3,1,2,-6,5,-8,6], 0)
 print(blah)
